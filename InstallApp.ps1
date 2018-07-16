a3af5566e78ca456d6db8b1b4c96b6da493b2818
Adnan Mulalic <adnanmu@maestralsolutions.com>
2018-07-16 09:03:48 +0200

    Add logs to InstallApp.ps1

--- InstallApp.ps1
+++ InstallApp.ps1
@@ -1,10 +1,28 @@
+$Logfile = "C:\logs\$(gc env:computername).log"
+
+Function LogWrite
+{
+   Param ([string]$logstring)
+
+   Add-content $Logfile -value $logstring
+}
+
+LogWrite('========== Start executing InstallApp script ==========')
+
 sl C:\ExploringAspNetCore\alinac
+
+LogWrite('========== START: restore ==========')
 
 # Restore the nuget references
 & "C:\Program Files\dotnet\dotnet.exe" restore
 
+LogWrite('========== START: publish ==========')
+
+
 # Publish application with all of its dependencies and runtime for IIS to use
 & "C:\Program Files\dotnet\dotnet.exe" publish --configuration release -o c:\ExploringAspNetCore\alinac\publish --runtime active
+
+LogWrite('========== START: IIS config ==========')
 
 
 # Point IIS wwwroot of the published folder. CodeDeploy uses 32 bit version of PowerShell.
