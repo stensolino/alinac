abc3afec7a1046a8eaaf38cacd943832b2609d23
Adnan Mulalic <stensolino@hotmail.com>
2018-07-17 22:27:34 +0200

    Update folder and files structure

--- InstallApp.ps1
+++ InstallApp.ps1
@@ -9,7 +9,7 @@ Function LogWrite
 
 LogWrite('========== Start executing InstallApp script ==========')
 
-sl C:\ExploringAspNetCore\alinac
+sl C:\ExploringAspNetCore
 
 #LogWrite('========== START: restore ==========')
 
@@ -22,12 +22,23 @@ sl C:\ExploringAspNetCore\alinac
 # Publish application with all of its dependencies and runtime for IIS to use
 #& "C:\Program Files\dotnet\dotnet.exe" publish --configuration release -o c:\ExploringAspNetCore\alinac\publish #--runtime active
 
-LogWrite('========== START: IIS config ==========')
+LogWrite('========== START: Copy and move files ==========')
 
+Remove-Item .\fappspec.yml -Force
+Remove-Item .\InstallApp.ps1 -Force
+Remove-Item .\RemoveApp.ps1 -Force
+
+Copy-Item -Path .\alinac\release -Recurse -Destination .\ -Container
+
+Remove-Item .\alinac -Force -Recurse
+
+LogWrite('========== END: Copy and move files ==========')
+
+LogWrite('========== START: IIS config ==========')
 
 # Point IIS wwwroot of the published folder. CodeDeploy uses 32 bit version of PowerShell.
 # To make use the IIS PowerShell CmdLets we need call the 64 bit version of PowerShell.
 #C:\Windows\SysNative\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Module WebAdministration; Set-ItemProperty 'IIS:\sites\Default Web Site' -Name physicalPath -Value c:\ExploringAspNetCore\publish}
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Module WebAdministration; Set-ItemProperty 'IIS:\sites\Default Web Site' -Name physicalPath -Value C:\ExploringAspNetCore\alinac\publish}
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Module WebAdministration; Set-ItemProperty 'IIS:\sites\Default Web Site' -Name physicalPath -Value C:\ExploringAspNetCore\alinac}
 
 LogWrite('========== END: IIS config ==========')
